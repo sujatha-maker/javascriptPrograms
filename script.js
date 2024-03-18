7c87444b9cf580c8db66c5da0c9a1fbef28f80a5
ArthiH <arthihari398@gmail.com>
2024-03-18 11:03:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// function name() {
+//   let a = "bob";
+//   console.log("hello", a);
+// }
+
+// let a = "hi";
+// let b = "suja";
+// function abab() {
+//   console.log(a+b+b+a);
+// }
+// abab();
+
+// function concat(text1, text2) {
+//   let value = `<${text1}>${text2}</${text1}>`;
+//   return value;
+// }
+// console.log(concat("b", "sujatha"));
+
