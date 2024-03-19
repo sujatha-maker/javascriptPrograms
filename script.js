b8ad99ef69be18b7bf18fbffeaa8ae4ace8300fb
= <=>
2024-03-19 11:18:56 +0530

    programupdate

--- script.js
+++ script.js
@@ -16,3 +16,29 @@
 // }
 // console.log(concat("b", "sujatha"));
 
+// function add(tag, text) {
+//   let value = tag.slice(2, 4);
+//   let last = tag.slice(0, 2);
+//   let ans = `${last}${text}${value}`;
+//   return ans;
+// }
+// console.log(add("<<>>", "suja"));
+
+// function copy(text) {
+//   let value = text.slice(3, 5);
+//   let ans = value.repeat(3);
+//   return ans;
+// }
+// console.log(copy("hello"));
+
+// function copy(text) {
+//   let value = text.substring(0, 2);
+//   return value;
+// }
+// console.log(copy("Hello"));
+
+function copy(text) {
+  let value = text.length / 2;
+  return value;
+}
+console.log(copy("Helloo"));
